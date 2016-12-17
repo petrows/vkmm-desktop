b5be66c5ddcde9891bd0091685d0ae461a18b92a
Petro <petro@petro.ws>
2016-12-17 19:16:45 +0300

    fix build

--- src/config.h
+++ src/config.h
@@ -1,7 +1,8 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define APP_ID			2929902
+//#define APP_ID			2929902
+#define APP_ID			28909846
 
 #define LASTFM_KEY		"b4d8e1be8070c399fb856f9a4cf9b901"
 #define LASTFM_SECRET	"249915a2df770062b7caf4ff827fc9cb"
