6bd4a718e1a113a021798bfcad79c1c75a3542f0
Petro <petro@petro.ws>
2016-12-17 19:32:01 +0300

    added windows app_id key

--- src/config.h
+++ src/config.h
@@ -2,7 +2,8 @@
 #define CONFIG_H
 
 //#define APP_ID			2929902
-#define APP_ID			28909846
+//#define APP_ID			28909846
+#define APP_ID			3697615
 
 #define LASTFM_KEY		"b4d8e1be8070c399fb856f9a4cf9b901"
 #define LASTFM_SECRET	"249915a2df770062b7caf4ff827fc9cb"
